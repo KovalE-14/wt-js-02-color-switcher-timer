9fa999281a142b3998094e95c114b1101263dad9
KovalE-14 <mailforassistent@gmail.com>
2025-03-23 23:41:35 +0100

    new code

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -54,31 +54,30 @@ function onBtnStartClick() {
   }, 1000);
 }
 
-// додати функцію підрахунку значень у мілісекундах
+
 function convertMs(ms) {
-  // Кількість мілісекунд на одиницю часу
+
   const second = 1000;
   const minute = second * 60;
   const hour = minute * 60;
   const day = hour * 24;
 
-  // Залишилось днів
+
   const days = Math.floor(ms / day);
-  // Залишилось годин
+ 
   const hours = Math.floor((ms % day) / hour);
-  // Залишилось хвилин
+  
   const minutes = Math.floor(((ms % day) % hour) / minute);
-  // Залишилось секунд
+  
   const seconds = Math.floor((((ms % day) % hour) % minute) / second);
 
   return { days, hours, minutes, seconds };
 }
 
-console.log(convertMs(2000)); // {days: 0, hours: 0, minutes: 0, seconds: 2}
-console.log(convertMs(140000)); // {days: 0, hours: 0, minutes: 2, seconds: 20}
-console.log(convertMs(24140000)); // {days: 0, hours: 6 minutes: 42, seconds: 20}
+console.log(convertMs(2000)); 
+console.log(convertMs(140000)); 
+console.log(convertMs(24140000)); 
 
-// додати функцію для встановлення 0 для значень таймера
 
 function addLeadingZero(value) {
   return `${value}`.padStart(2, '0');
